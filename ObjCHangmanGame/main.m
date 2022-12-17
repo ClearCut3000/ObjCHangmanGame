9a451640c0ae1d041a3c8b8cc7fee9d0307770e0
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-18 00:27:59 +0300

    Initial Commit

---
+++ ObjCHangmanGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCHangmanGame
+//
+//  Created by Николай Никитин on 18.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
